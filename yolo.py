54001c90b70fd1b083190f7b6b7cee2d5070c046
thuaduc24042001 <thuaduc24042001@gmail.com>
2025-03-14 11:21:43 +0100

    small adjustment

--- yolo.py
+++ yolo.py
@@ -1,9 +1,19 @@
+import torch
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+# Check if CUDA is available
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Train the model with MPS
-results = model.train(
-    data="config.yaml", epochs=100, imgsz=640, device="cuda", batch=16, patience=10
-)
+if device == "cuda":
+    print("CUDA is available. Running on GPU.")
+
+    # Load a model
+    model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+    model.to(device)
+
+    # Train the model
+    results = model.train(
+        data="config.yaml", epochs=100, imgsz=640, device=device, batch=16, patience=10
+    )
+else:
+    print("CUDA is not available. Exiting.")
